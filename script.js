f63b14c67919c4203b0ea78d6ee4037264f9f9c0
Ordth <braillard.thomas@gmail.com>
2024-03-07 11:00:10 +0100

    Created a tab in js and print the first and last element in the console

---
+++ script.js
@@ -0,0 +1,4 @@
+let VegetablesList =["Carrot","Beet", "Eggplant", "Cauliflower", "Asparagus", "Bok choy", "Garlic", "Onion", "Pumpkin", "Potato", "Broccoli" ]
+
+console.log(VegetablesList[0])
+console.log(VegetablesList[10])
